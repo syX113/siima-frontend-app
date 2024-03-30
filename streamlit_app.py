a53e93d99decf5f2fc5868d80871ec108e040f43
Tim Giger <tim.giger@swisscom.com>
2024-03-30 11:34:43 +0100

    first table data display

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,17 +7,12 @@ mongo_connection_string = os.getenv("MONGO_CONNECTION_STRING")
 
 # Connect to the MongoDB client
 client = MongoClient(mongo_connection_string)
-
-# Connect to your database - replace 'your_database_name' with your actual database name
 db = client['cosmos-db-siima-telemetry']
-
-# Specify the collection - replace 'your_collection_name' with your actual collection name
 collection = db['cosmos-collection-siima-telemetry']
 
 # Fetch data from the collection
-# This example fetches all documents from the specified collection, you can modify the query as needed
-data = list(collection.find())
+data = list(collection.find().limit(10))
 
 # Display the data in a table
-st.title('MongoDB Data Display')
+st.title('Energy Account - Overview')
 st.table(data)
