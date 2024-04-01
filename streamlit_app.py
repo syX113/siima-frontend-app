57055fe6da3c8f770c591f689139466484a5a143
Tim Giger <tim.giger93@gmail.com>
2024-04-01 13:46:15 +0200

    revised calculations and logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,91 +1,68 @@
 import streamlit as st
 import streamlit_authenticator as stauth
-from pymongo import MongoClient
 import pandas as pd
 import altair as alt
 import os
-import yaml
-from yaml.loader import SafeLoader
+from pymongo import MongoClient
 
 def authenticate():
-
     authenticator = stauth.Authenticate(
         st.secrets["credentials"].to_dict(),
         st.secrets['cookie']['name'],
         st.secrets['cookie']['key'],
         st.secrets['cookie']['expiry_days']
     )
-    
-    user_collection_map = st.secrets["user_collection_map"].to_dict()
-    return authenticator, user_collection_map
+    return authenticator, st.secrets["user_collection_map"]["mapping"]
 
 def calculate_energy_balance(df):
-
-    # Initialize the 'Energy_Account_Balance_kW' column
-    df['Energy_Account_Balance_kW'] = 0.0
-
-    # Perform the rolling calculation
-    for i in range(1, len(df)):
-        previous_balance = df.iloc[i - 1]['Energy_Account_Balance_kW']
-        previous_net = df.iloc[i - 1]['SmartMeter_Consumption_B1_kW'] - df.iloc[i - 1]['SmartMeter_Production_E1_kW']
-        current_net = df['SmartMeter_Consumption_B1_kW'].iloc[i] - df['SmartMeter_Production_E1_kW'].iloc[i]
-        df.at[i, 'Energy_Account_Balance_kW'] = previous_balance + (previous_net - current_net)
+    # Calculate net consumption/production for each row
+    df['Net_Consumption_Production_kW'] = df['SmartMeter_Consumption_B1_kW'] - df['SmartMeter_Production_E1_kW']
+    
+    # Calculate the difference in net consumption/production between consecutive rows
+    df['Delta_Net_kW'] = df['Net_Consumption_Production_kW'].diff().fillna(0)
+    
+    # Reverse the sign of the delta to match the original calculation logic
+    df['Delta_Net_kW'] *= -1
+    
+    # Cumulatively sum these deltas to get the energy account balance
+    df['Energy_Account_Balance_kW'] = df['Delta_Net_kW'].cumsum()
     
     return df
 
-def fetch_data(collection_name):
 
-    mongo_connection_string = os.getenv("MONGO_CONNECTION_STRING")
-
-    client = MongoClient(mongo_connection_string)
-
+def fetch_data(client, collection_name):
     db = client['cosmos-db-siima-telemetry']
-
     collection = db[collection_name]
-    projection = {  '_id': 0, # Exclude
-                    'DeviceMessageTimestamp': 1, 
-                    'SmartMeter_Consumption_B1_kW': 1, 
-                    'SmartMeter_Production_E1_kW': 1,
-                    'Current_Total_Input_W': 1, 
-                    'Current_Total_Output_W': 1, 
-                    'Current_Phase1_Input_W': 1, 
-                    'Current_Phase2_Input_W': 1, 
-                    'Current_Phase3_Input_W': 1, 
-                    'Current_Phase1_Output_W': 1, 
-                    'Current_Phase2_Output_W': 1, 
-                    'Current_Phase3_Output_W': 1
-                }
-    
+    projection = {
+        '_id': 0, 'DeviceMessageTimestamp': 1, 
+        'SmartMeter_Consumption_B1_kW': 1, 'SmartMeter_Production_E1_kW': 1,
+        'Current_Total_Input_W': 1, 'Current_Total_Output_W': 1
+    }
     data = list(collection.find(projection=projection).sort('DeviceMessageTimestamp', 1))
 
     df = pd.DataFrame(data)
     # Convert 'DeviceMessageTimestamp' to datetime
     df['DeviceMessageTimestamp'] = pd.to_datetime(df['DeviceMessageTimestamp'], unit='ms')
-
     # Localize timestamps to "Europe/Zurich" without converting from UTC
     df['DeviceMessageTimestamp'] = df['DeviceMessageTimestamp'].dt.tz_localize('Europe/Zurich', ambiguous='raise')
-
     df = df[['DeviceMessageTimestamp', 'SmartMeter_Consumption_B1_kW', 'SmartMeter_Production_E1_kW', 'Current_Total_Input_W', 'Current_Total_Output_W']]
 
-    
     return df
-
-### Main Logic ###
 
 # Authenticate user
 authenticator, user_collection_map = authenticate()
-_, authentication_status, username = authenticator.login(fields={'Form name':'Login', 'Username':'Username', 'Password':'Password', 'Login':'Login'})
+_, authentication_status, username = authenticator.login(fields={'Form name':'Siima Login', 'Username':'Username', 'Password':'Password', 'Login':'Login'})
 
-# User is authenticated
+# Check if user is authenticated
 if authentication_status:
 
     st.write(f'Welcome *{st.session_state["name"]}*')
     st.title('Siima | Swiss Energy Account')
+    collection_name = user_collection_map.get(username, "default_collection_name")
 
-    collection_mapping = user_collection_map.get('mapping', {})
-    collection_name = collection_mapping.get(username, "default_collection_name")
+    client = MongoClient(os.getenv("MONGO_CONNECTION_STRING"))
+    df = fetch_data(client, collection_name)
 
-    df = fetch_data(collection_name)
     df = calculate_energy_balance(df)
 
     if not df.empty:
@@ -108,12 +85,13 @@ if authentication_status:
             delta = last_balance_value - previous_balance_value
 
         # Assuming 'first_timestamp' is your datetime object
-        date_str = first_timestamp.strftime('%d. %B %Y')  # Basic formatting
+        date_str = first_timestamp.strftime('%d. %B %Y')
 
         # Removing leading zero from day and adding ordinal suffix
         formatted_date = first_timestamp.strftime("%-d. %B %Y")
-        time_str = first_timestamp.strftime('%H:%M')  # Rounds to the nearest minute
-        # Use st.columns to display KPIs side by side
+        time_str = first_timestamp.strftime('%H:%M') # Round to minute
+
+        # Setup KPI row
         col1, col2, col3 = st.columns([4, 4, 4])
 
         # Current energy balance
@@ -123,10 +101,10 @@ if authentication_status:
         # 6 hour delta and change
         with col2:
             if len(df) > 360:
-                st.metric(label="**Energy Account Balance 12h ago:**", value=f"{previous_balance_value:.2f} kW", delta=f"{delta:.2f} kW to Now")
+                st.metric(label="**Energy Account Balance 12h Ago:**", value=f"{previous_balance_value:.2f} kW", delta=f"{delta:.2f} kW to Now")
 
+        # Show timestamp if first datapoint
         with col3:
-            # Attempting to nudge the title up with a negative margin
             st.markdown(f"""
             <div style="margin-top: -0px;">
                 <span style="font-size: 0.85em; font-weight: bold;">Smart Meter Data Received from:</span><br>
@@ -135,7 +113,8 @@ if authentication_status:
             {time_str}
             """, unsafe_allow_html=True)
 
-        st.markdown('<hr style="border-top-color: #ffffff; border-top-width: 2px;"/>', unsafe_allow_html=True)
+        # Simple horizontal devider line
+        st.markdown('<hr style="border-top-color: #ffffff; border-top-width: 10px;"/>', unsafe_allow_html=True)
 
         # Chart for the Energy Account Balance
         balance_chart = alt.Chart(df).mark_line(color='#42c0b1').encode(
@@ -148,12 +127,12 @@ if authentication_status:
             title='Energy Account Balance (kW)'
         ).interactive()
 
-        # Display the chart in Streamlit
         st.altair_chart(balance_chart, use_container_width=True)
 
-        st.markdown('<hr style="border-top-color: #ffffff; border-top-width: 2px;"/>', unsafe_allow_html=True)
+        # Simple horizontal devider line
+        st.markdown('<hr style="border-top-color: #ffffff; border-top-width: 10px;"/>', unsafe_allow_html=True)
 
-        # Define the base chart with common properties
+        # Chart for "Feed In" and "From Grid"
         base = alt.Chart(df).properties(
             width=800,
             height=400,
@@ -180,7 +159,7 @@ if authentication_status:
         chart = alt.layer(input_line, output_line).resolve_scale(y='shared').properties().interactive()
 
         # Add a custom legend
-        legend_entries = ['From Grid', 'Feed In']
+        legend_entries = ['From Grid (Consumption)', 'Feed In (Production)']
         colors = ['#FF9B9B', '#8FD694']
 
         # Display the chart
@@ -188,12 +167,12 @@ if authentication_status:
         legend_html = "".join([f"<span style='color:{color}; margin-right: 20px;'>● {legend}</span>" for legend, color in zip(legend_entries, colors)])
         st.markdown(f"<div style='text-align: left;'>{legend_html}</div>", unsafe_allow_html=True)
 
-    st.markdown('<hr style="border-top-color: #ffffff; border-top-width: 5px;"/>', unsafe_allow_html=True)
+    st.markdown('<hr style="border-top-color: #ffffff; border-top-width: 10px;"/>', unsafe_allow_html=True)
 
     # Render logout button
     authenticator.logout()
 
-# Login failed or no credentials entered
+# Login failed or no credentials entered if user is not authenticated
 elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
 
